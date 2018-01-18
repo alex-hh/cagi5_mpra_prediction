6adeaae9fd826e289ba7d442b103ca3e93da6c7f
Alex Hawkins <alexh@berlioz>
2018-01-18 15:12:17 +0000

    setup for loading our own pretrained models

--- models.py
+++ models.py
@@ -1,7 +1,8 @@
+import pickle
 import numpy as np
 import xgboost as xgb
 
-from utils import snp_feats_from_preds
+from utils import snp_feats_from_preds, encode_sequences
 from sklearn.linear_model import LogisticRegression
 from sklearn.utils.class_weight import compute_sample_weight
 
@@ -43,5 +44,40 @@ class DeepSeaSNP:
 
 
 class DSDataKerasModel:
-  def __init__(self, experiment_name):
-    pass
+  def __init__(self, experiment_name, feattypes=['absdiff'],
+               verbose=False, multiclass='ovr', classifier='lr', classifier_kwargs={}):
+    self.feattypes = feattypes
+    self.verbose = verbose
+    self.multiclass = multiclass
+    self.classifier_kwargs = classifier_kwargs
+    self.classifier = classifier
+    self.experiment_name = experiment_name
+
+  def get_features(self, df):
+    assert 'ref_sequence' in df.columns
+    ref_onehot = encode_sequences(df['ref_sequence'], seqlen=1000)
+    alt_onehot = encode_sequences(df['alt_sequence'], seqlen=1000)
+    self.get_trained_model()
+
+    if all_layers:
+      m = model_class.model
+      ref_p = m.predict(ref_onehot)
+      alt_p = m.predict(alt_onehot)
+      pass
+
+    else:
+      pass
+
+  def get_trained_model(self):
+    model_class = self.get_untrained_model(self.experiment_name)
+    model_class.get_compiled_model()
+    model_class.model.load_weights('data/remote_results/models-best/{}.h5'.format(RESULT_DIR, experiment_name))
+    self.model_class = model_class
+
+  def get_untrained_model(self):
+    settings = pickle.load(open('data/remote_workspace/experiment_settings/{}.p'.format(self.experiment_name), 'rb'))
+    # if i just want the class name i can do settings['model_class'].__name__
+    model_class, model_args = settings['model_class'], settings['model_args'] 
+    if 'reg_weighting' in model_args: 
+      model_args['reg_weighting'] = get_weighting(**model_args['reg_weighting'])
+    return model_class(**model_args)
