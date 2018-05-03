ecdd2af8428242fc274b90d8f0ebc3a67332433b
alex-hh <alex.hhooker@gmail.com>
2018-05-03 20:30:49 +0200

    save single layer

--- save_model_preds.py
+++ save_model_preds.py
@@ -9,7 +9,25 @@ from sequence_feats import seqfeats_from_df
 from utils import get_seqs_and_inds
 # absdiff = snpfeats_from_df(df, 1000)
 
-def main(seqfeatextractor, alllayers=False, dataset='train', seqlen=1000, v=2):
+def main(seqfeatextractor, layer=None, alllayers=False, dataset='train', seqlen=1000, v=2):
+  if layer > 0:
+    assert not alllayers
+    layers = [layer]
+  else:
+    if seqfeatextractor == 'deepsea' and alllayers:
+      layers = ['2','6','9','13','15']
+    elif seqfeatextractor == 'deepsea':
+      layers = ['15']
+    elif seqfeatextractor == 'danqpy2':
+      pass
+    elif re.search('dq', seqfeatextractor):
+      if alllayers:
+        layers = [3,5,11]
+      else:
+        layers = []
+    else:
+      layers = []
+
   print('Saving preds from {} model on {} set using seqs of len {} seqs v {}'.format(seqfeatextractor, dataset, seqlen, v))
   if dataset == 'train':
     df = pd.read_csv('data/cagi5_df.csv')
@@ -25,19 +43,7 @@ def main(seqfeatextractor, alllayers=False, dataset='train', seqlen=1000, v=2):
   assert list(df['Refcheck']) == list(df['Ref'])
   assert list(df['Altcheck']) == list(df['Alt'])
 
-  if seqfeatextractor == 'deepsea' and alllayers:
-    layers = ['2','6','9','13','15']
-  elif seqfeatextractor == 'deepsea':
-    layers = ['15']
-  elif seqfeatextractor == 'danqpy2':
-    pass
-  elif re.search('dq', seqfeatextractor):
-    if alllayers:
-      layers = [3,5,11]
-    else:
-      layers = []
-  else:
-    layers = []
+  
 
   print('getting preds', flush=True)
   refp, altp = seqfeats_from_df(df, use_gpu=False, seqlen=seqlen, # target sequence length - i.e. how much padding should be applied
@@ -45,8 +51,8 @@ def main(seqfeatextractor, alllayers=False, dataset='train', seqlen=1000, v=2):
   print('ref preds shape', refp.shape, flush=True)
 
   suffix = ''
-  if alllayers:
-    suffix = '-all'
+  if layers:
+    suffix = '-'+'-'.join(layers)
   if dataset == 'test':
     suffix += '-test'
 
@@ -58,6 +64,7 @@ if __name__ == '__main__':
   parser.add_argument("seqfeatextractor", help="Name of model to use to generate preds")
   parser.add_argument("--test", action='store_true')
   parser.add_argument("seqlen", nargs='?', default=1000, type=int)
+  parser.add_argument("--layer", default=0, type=int)
   parser.add_argument("--v", default=2, type=int)
   parser.add_argument('--alllayers', action='store_true')
   args = parser.parse_args()
